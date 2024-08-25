48878ab5fd621b90ad3c33cdcb9771bcf887e90e
Furka <Furka@FURKAN>
2024-08-25 14:36:24 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Akaryakit_Otomasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1021, 586);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "BENZİN (95)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "BENZİN (97)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(243, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(243, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(386, 58);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(164, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(386, 128);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(164, 23);
+            this.progressBar2.TabIndex = 8;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(606, 61);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 9;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(606, 131);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 10;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(38, 191);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(809, 431);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.numericUpDown2);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.progressBar2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(801, 405);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(801, 405);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(801, 405);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(966, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(919, 111);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(177, 154);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 644);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Akaryakit_Otomasyonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Font = new Font(this.Font, FontStyle.Bold);
+            this.Load+=Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            tabControl1.Location = new Point(10,10);
+            label1.Font = new Font(label1.Font, FontStyle.Bold);
+            this.Text = "AKARYAKIT OTOMASYONU";
+            progressBar1.Maximum = 200;
+            progressBar2.Maximum = 200;
+            //tabPage1.Font = new Font(tabPage1.Font,FontStyle.Bold);
+            tabControl1.Font = new Font(tabControl1.Font,FontStyle.Bold);
+            tabPage1.Text = "  DEPO BİLGİLERİ  ";
+            tabPage2.Text = "  FİYAT BİLGİLERİ ";
+            tabPage3.Text = "  SATIŞ YAP       ";
+            number1 = Convert.ToInt32(number);
+            toplam = number1 * 5;
+            //pictureBox1.Image = Image.FromFile(@"Images\a.bmp");
+          
+           pictureBox1.BackColor = Color.Orange;
+            pictureBox1.SizeMode =PictureBoxSizeMode.StretchImage;
+            //pictureBox1.Image = Image.FromFile("free.png");
+            //pictureBox1.Image = Image.FromFile("picture\\free.png");
+            pictureBox1.Image = Image.FromFile(@"picture\free.png");
+
+            //label2.Text = System.IO.Directory.GetCurrentDirectory();
+             string resim = System.IO.Directory.GetCurrentDirectory();
+
+            //Image image =Image.FromFile(@"Debug\free.png"); 
+            //Path.GetFullPath(PicPath)
+            //C:\Users\Furka\source\repos\Akaryakit_Otomasyonu\bin\Debug
+            //label2.Text = System.IO.Path.GetFileName(resim);
+            //label2.Text = System.IO.Path.GetFileName("C:\\Users\\Furka\\source\\repos\\Akaryakit_Otomasyonu\\bin\\Debug");
+            label2.Text = System.IO.Path.GetFileName("C:\\Users\\Furka\\source\\repos\\Akaryakit_Otomasyonu\\bin");
+
+        }
+
+        double D_benzin95 = 0, D_benzin97 = 0;
+        double E_benzin95 = 0, E_benzin97 = 0;
+        double F_benzin95 = 0, F_benzin97 = 0;
+        double S_benzin95 = 0, S_benzin97 = 0;
+
+        string number = "35";
+        int number1 = 0;
+        int toplam = 0;
+        int yeniTop = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt_depo_oku();
+            txt_depo_yaz();
+            txt_fiyat_oku();
+            txt_fiyat_yaz();
+            progressBar_guncelle();
+            numericupdown_value();
+        }
+
+        string[] depo_bilgileri;
+        string[] fiyat_bilgileri;
+
+        private void txt_depo_oku()
+        {
+            depo_bilgileri = System.IO.File.ReadAllLines(Application.StartupPath + "\\klasor\\depo.txt");
+            //depo_bilgileri = System.IO.File.ReadAllLines(Application.StartupPath + "\\depo.txt");
+            //depo_bilgileri = System.IO.File.ReadAllLines(@"C: \Users\Furka\source\repos\Akaryakit_Otomasyonu\bin\Debug\klasor\depo.txt"); HATALI
+            D_benzin95 = Convert.ToDouble(depo_bilgileri[0]);
+            D_benzin97 = Convert.ToDouble(depo_bilgileri[1]);
+
+        
+        }
+
+        private void txt_depo_yaz()
+        {
+            label1.Text = D_benzin95.ToString("N");
+            label2.Text = D_benzin97.ToString("N");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            yeniTop = Int16.Parse(number);
+            yeniTop = yeniTop + 1;
+            label1.Text = yeniTop.ToString();
+        }
+
+        private void txt_fiyat_oku()
+        {
+            fiyat_bilgileri = System.IO.File.ReadAllLines(Application.StartupPath + "\\klasor\\fiyat.txt");
+            F_benzin95 = Convert.ToDouble(fiyat_bilgileri[0]);
+            F_benzin97 = Convert.ToDouble(fiyat_bilgileri[1]);
+        }
+
+        private void txt_fiyat_yaz()
+        {
+            textBox1.Text = F_benzin95.ToString("N");
+            textBox2.Text = F_benzin97.ToString("N");
+        }
+
+        private void progressBar_guncelle()
+        {
+            progressBar1.Value = Convert.ToInt16(D_benzin95);
+            progressBar2.Value = Convert.ToInt16(D_benzin97);
+        }
+
+        private void numericupdown_value()
+        {
+            //numericUpDown1.Maximum = decimal.Parse(numericUpDown1.Value.ToString());
+            numericUpDown1.Maximum = decimal.Parse(D_benzin95.ToString());
+            numericUpDown2.Maximum = decimal.Parse(D_benzin97.ToString());
+        }
+
+    }
+}
